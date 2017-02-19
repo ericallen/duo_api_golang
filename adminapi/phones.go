17d4363bc1dab2becf9cea75b15e7b51e70e1084
Eric Allen <eric.allen@usu.edu>
2017-02-19 13:36:14 -0700

    Added the rest of the phone methods

--- adminapi/phones.go
+++ adminapi/phones.go
@@ -41,6 +41,25 @@ type PhoneUser struct {
 	Username   string
 }
 
+type PhoneIDResult struct {
+	Stat     string
+	Response PhoneResponse
+}
+
+func SetNumber(number string) func(*url.Values) {
+	return func(opts *url.Values) {
+		opts.Set("number", number)
+	}
+}
+
+func SetExtension(extension string) func(*url.Values) {
+	return func(opts *url.Values) {
+		opts.Set("extension", extension)
+	}
+}
+
+//Retrieve Phones
+//Optional parameters SetNumber and SetExtension
 func (api *AdminApi) RetrievePhones() (*PhoneUserResult, error) {
 	opts := url.Values{}
 
@@ -55,6 +74,94 @@ func (api *AdminApi) RetrievePhones() (*PhoneUserResult, error) {
 	return ret, err
 }
 
+func SetName(name string) func(*url.Values) {
+	return func(opts *url.Values) {
+		opts.Set("name", name)
+	}
+}
+
+func SetType(typestring string) func(*url.Values) {
+	return func(opts *url.Values) {
+		opts.Set("type", typestring)
+	}
+}
+
+func SetPlatform(platform string) func(*url.Values) {
+	return func(opts *url.Values) {
+		opts.Set("platform", platform)
+	}
+}
+
+func SetPredelay(predelay string) func(*url.Values) {
+	return func(opts *url.Values) {
+		opts.Set("predelay", predelay)
+	}
+}
+
+func SetPostdelay(postdelay string) func(*url.Values) {
+	return func(opts *url.Values) {
+		opts.Set("postdelay", postdelay)
+	}
+}
+
+//Create Phone
+//Optional parameters SetNumber, SetName, SetExtension, SetType, SetPlatform, SetPredelay and SetPostdelay
+func (api *AdminApi) CreatePhone(options ...func(*url.Values)) (*PhoneIDResult, error) {
+	opts := url.Values{}
+	for _, o := range options {
+		o(&opts)
+	}
+	_, body, err := api.SignedCall("POST", "/admin/v1/phones", opts, duoapi.UseTimeout)
+	if err != nil {
+		return nil, err
+	}
+	ret := &PhoneIDResult{}
+	if err = json.Unmarshal(body, ret); err != nil {
+		return nil, err
+	}
+	return ret, nil
+}
+
+//Retrieve Phone by ID
+//Required parameters - phone_id
+//Optional parameters - None
+func (api *AdminApi) RetrievePhoneByID(phone_id string) (*PhoneIDResult, error) {
+	opts := url.Values{}
+	path := fmt.Sprintf("/admin/v1/phones/%s", phone_id)
+	_, body, err := api.SignedCall("GET", path, opts, duoapi.UseTimeout)
+	if err != nil {
+		return nil, err
+	}
+	ret := &PhoneIDResult{}
+	if err = json.Unmarshal(body, ret); err != nil {
+		return nil, err
+	}
+	return ret, nil
+}
+
+//Modify Phone
+//Required parameters - phone_id
+//Optional parameters - SetNumber, SetName, SetExtension, SetType, SetPlatform, SetPredelay, SetPostdelay
+func (api *AdminApi) ModifyPhone(phone_id string, options ...func(*url.Values)) (*PhoneIDResult, error) {
+	opts := url.Values{}
+	for _, o := range options {
+		o(&opts)
+	}
+	path := fmt.Sprintf("/admin/v1/phones/%s", phone_id)
+	_, body, err := api.SignedCall("POST", path, opts, duoapi.UseTimeout)
+	if err != nil {
+		return nil, err
+	}
+	ret := &PhoneIDResult{}
+	if err = json.Unmarshal(body, ret); err != nil {
+		return nil, err
+	}
+	return ret, nil
+}
+
+//Delete Phone
+//Required parameters - phone_id
+//Optional parameters - None
 func (api *AdminApi) DeletePhone(phone_id string) (*SimpleResponse, error) {
 	opts := url.Values{}
 	path := fmt.Sprintf("/admin/v1/phones/%s", phone_id)
@@ -67,4 +174,134 @@ func (api *AdminApi) DeletePhone(phone_id string) (*SimpleResponse, error) {
 		return nil, err
 	}
 	return ret, err
+}
+
+func SetValidSeconds(valid_seconds string) func(*url.Values) {
+	return func(opts *url.Values) {
+		opts.Set("valid_secs", valid_seconds)
+	}
+}
+
+func SetInstall(install string) func(*url.Values) {
+	return func(opts *url.Values) {
+		opts.Set("install", install)
+	}
+}
+
+type ActivationCodeResponse struct {
+	Activation_URL     string
+	Activation_Barcode string
+	Installation_URL   string
+	Valid_Secs         int64
+}
+
+type ActivationCodeResult struct {
+	Stat     string
+	Response ActivationCodeResponse
+}
+
+//Create Activiation Code
+//Required parameters - phone_id
+//Optional parameters - SetValidSeconds, SetInstall
+func (api *AdminApi) CreateActivationCode(phone_id string, options ...func(*url.Values)) (*ActivationCodeResult, error) {
+	opts := url.Values{}
+	for _, o := range options {
+		o(&opts)
+	}
+	path := fmt.Sprintf("/admin/v1/phones/%s/activation_url", phone_id)
+	_, body, err := api.SignedCall("POST", path, opts, duoapi.UseTimeout)
+	if err != nil {
+		return nil, err
+	}
+	ret := &ActivationCodeResult{}
+	if err = json.Unmarshal(body, ret); err != nil {
+		return nil, err
+	}
+	return ret, err
+}
+
+type ActivationCodeSMSResult struct {
+	Stat     string
+	Response struct {
+		Activation_Barcode string
+		Activation_Msg     string
+		Installation_Msg   string
+		Valid_Secs         int64
+	}
+}
+
+func SetInstallationMsg(installation_msg string) func(*url.Values) {
+	return func(opts *url.Values) {
+		opts.Set("install", installation_msg)
+	}
+}
+
+func SetActivationMsg(activation_msg string) func(*url.Values) {
+	return func(opts *url.Values) {
+		opts.Set("install", activation_msg)
+	}
+}
+
+//Send Activiation Code by SMS
+//Required parameters - phone_id
+//Optional parameters - SetValidSeconds, SetInstall, SetInstallationMsg, SetActivationMsg
+func (api *AdminApi) SendActivationCodebySMS(phone_id string, options ...func(*url.Values)) (*ActivationCodeSMSResult, error) {
+	opts := url.Values{}
+	for _, o := range options {
+		o(&opts)
+	}
+	path := fmt.Sprintf("/admin/v1/phones/%s/send_sms_activation", phone_id)
+	_, body, err := api.SignedCall("POST", path, opts, duoapi.UseTimeout)
+	if err != nil {
+		return nil, err
+	}
+	ret := &ActivationCodeSMSResult{}
+	if err = json.Unmarshal(body, ret); err != nil {
+		return nil, err
+	}
+	return ret, nil
+}
+
+type InstallationURLviaSMSResult struct {
+	Stat     string
+	Response struct {
+		Installation_Msg string
+	}
+}
+
+//Send Installational URL vis SMS
+//Required parameters - phone_id
+//Optional parameters - SetInstallationMsg
+func (api *AdminApi) SendInstallionalURLviaSMS(phone_id string, options ...func(*url.Values)) (*InstallationURLviaSMSResult, error) {
+	opts := url.Values{}
+	for _, o := range options {
+		o(&opts)
+	}
+	path := fmt.Sprintf("/admin/v1/phones/%s/send_sms_installation", phone_id)
+	_, body, err := api.SignedCall("POST", path, opts, duoapi.UseTimeout)
+	if err != nil {
+		return nil, err
+	}
+	ret := &InstallationURLviaSMSResult{}
+	if err = json.Unmarshal(body, ret); err != nil {
+		return nil, err
+	}
+	return ret, nil
+}
+
+//Send Passcodes via SMS
+//Required parameters - phone_id
+//Optional Parameters - None
+func (api *AdminApi) SendPasscodesviaSMS(phone_id string) (*SimpleResponse, error) {
+	opts := url.Values{}
+	path := fmt.Sprintf("/admin/v1/phones/%s/send_sms_passcodes", phone_id)
+	_, body, err := api.SignedCall("POST", path, opts, duoapi.UseTimeout)
+	if err != nil {
+		return nil, err
+	}
+	ret := &SimpleResponse{}
+	if err = json.Unmarshal(body, ret); err != nil {
+		return nil, err
+	}
+	return ret, nil
 }
